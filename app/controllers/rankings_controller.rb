e6b62b52be7aba7b4e0d567d8faf0ead7f27060c
tkawakami <tkawakami@tkltd.jp>
2015-09-01 13:09:26 +0000

    todo ranking

---
+++ app/controllers/rankings_controller.rb
@@ -0,0 +1,19 @@
+class RankingsController < ApplicationController
+  
+  def want
+    @hash = Want.group(:item_id).order('count_all desc').count
+    arr = Array.new()
+    @hash.each do |key, value|
+      @items = arr.push(Item.find(key))
+    end
+  end
+  
+  def have
+    @hash = Have.group(:item_id).order('count_all desc').count
+    arr = Array.new()
+    @hash.each do |key, value|
+      @items = arr.push(Item.find(key))
+    end
+  end
+  
+end
